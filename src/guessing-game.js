92a2237e9e8e44fac48b988038130b14062ec27e
AndreySmekhunov <105367492+AndreySmekhunov@users.noreply.github.com>
2022-08-16 23:34:16 +0300

    feat: solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    min;
+    max;
+    constructor() {
+
+    }
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
 
     }
 
     guess() {
+        return Math.ceil((this.max + this.min)/2);
 
     }
 
     lower() {
-
+ 
+        this.max = Math.ceil((this.max + this.min)/2)
     }
 
     greater() {
-
+        this.min = Math.ceil((this.max + this.min)/2)
     }
 }
 
